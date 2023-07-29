74863294dd40640e5d61d9df25fcb4d4e9ef8796
Svyatoslav Matyuha <svyatoslavmatyuha@gmail.com>
2023-07-29 19:47:53 +0300

    fix: order api erorrs

--- server/api/prisma/create-order.js
+++ server/api/prisma/create-order.js
@@ -9,18 +9,18 @@ export default defineEventHandler(async (event) => {
       userId: body.userId,
       stripeId: body.stripeId,
       name: body.name,
-      adress: body.address,
+      address: body.address,
       zipcode: body.zipcode,
       city: body.city,
       country: body.country
     }
   })
 
-  body.products.forEach(async (product) => {
-    await prisma.orderItems.create({
+  body.products.forEach(async (prod) => {
+    await prisma.orderItem.create({
       data: {
         orderId: order.id,
-        productId: Number(product.id)
+        productId: Number(prod.id)
       }
     })
   })
